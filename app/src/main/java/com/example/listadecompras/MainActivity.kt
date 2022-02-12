e41017dad2870b18f5e5253341d26362a1b9f072
Felipe Quevedo <motta.quevedo@ufrgs.br>
2022-02-11 22:23:50 -0300

    Add passos do capitulo 6.4
    - Adicionado a lógica da lista e do botão
    - Adicionado as instruções no Gradle para não precisar usar o findviewbyId

--- app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.listadecompras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Implementando o adaptador
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+
+        // definindo o adaptador na lista
+        lstProdutos.adapter = produtosAdapter
+
+        //definição do ouvinte do botão
+        btn_inserir.setOnClickListener {
+
+            // pegando o valor digitado pelo usuário
+            val produto = txt_produto.text.toString()
+
+            //enviado o item para a lista
+            produtosAdapter.add(produto)
+
+
+        }
+
+
     }
 }
